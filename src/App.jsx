84289b6aeb2eb6284c394b2e6f60f69ebf62f764
Zion livingstone <zionlivingstone4@gmail.com>
2023-05-21 11:36:01 +0100

    fix:modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable no-unused-vars */
-import { useEffect, useRef, useState,useLayoutEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {FaChevronDown,FaChevronUp,FaCheck,FaTimes} from 'react-icons/fa'
+import { Fragment } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+
 import axios from 'axios'
 
 function App() {
@@ -11,11 +15,29 @@ const pollwidth=useRef(null)
 const [userdata,Setuserdata]=useState([])
 const [data,setData]=useState([])
 const [poll,setPoll]=useState('')
+const [vote,setVote]=useState()
+
+const votes = [
+  { vote: 5,price: 500 },
+  { vote: 10, price: 1000 },
+  { vote: 15,price: 1500 },
+  { vote: 20,price: 2000 },
+  { vote: 30,price: 3000 },
+  { vote: 50,price: 5000 },
+]
+const [selected, setSelected] = useState(votes[0])
+const [price,setPrice]=useState(votes[0].price)
 
   function scale(num,inmin,inmax,outmin,outmax) {
     return(num-inmin)*(outmax-outmin)/(inmax-inmin)+outmin;
 }
-
+  function cancelModal(params) {
+    setVote()
+  }
+  function test(fee) {
+    setPrice(fee)
+  }
+  console.log(price);
   useEffect(()=>{
     // pollwidth.current.style.width=`${scale(data.percentage,0,100,0,poll)}px`
 
@@ -33,30 +55,24 @@ const [poll,setPoll]=useState('')
  
   },[])
 
-  useLayoutEffect(() => {
-    function updatePollWidth() {
-      if (pollref.current) {
-        setPoll(pollref.current.offsetWidth);
-      }
+useEffect(()=>{
+     if (pollref.current) {
+      setPoll(pollref.current.offsetWidth);
     }
-  
-    // Call the update function initially
-    updatePollWidth();
-  
-    // Attach an event listener to resize events
-    window.addEventListener('resize', updatePollWidth);
-  
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('resize', updatePollWidth);
-    };
-  }, [pollref.current]);
-  
+},[pollref.current])
+
+function votec(id) {
+  const choice=userdata.filter((data)=>data._id===id)
+  setVote(choice)
+}
   return (
-    <div className="h-full w-screen">
+    <div className={`h-full w-screen ${vote?' pointer-events-none  ':''}`}>
       <nav className='bg-secondary sticky top-0 left-0 w-full text-white text-2xl text-center py-2 z-50'>
         Face of Pharmacy
       </nav>
+      <div className={`${vote?' pointer-events-none blur-sm  ':''}`}>
+
+      
       <article className='my-8 mx-6 flex flex-col gap-4'>
       <div className="text-white text-xl">Poll Statistics</div>
             {/* /////////////poll-card///////////// */}
@@ -87,9 +103,9 @@ const [poll,setPoll]=useState('')
 
 
 
-{userdata.map((data)=>{
+{userdata.map((data,index)=>{
   return(
-    <>
+    < div key={index}>
      {/* ////////////contestant cards////////////// */}
      <div className="bg-secondary rounded-3xl overflow-hidden">
 
@@ -104,22 +120,87 @@ const [poll,setPoll]=useState('')
   <div className="flex px-6 py-2 text-white">
   <div className="flex flex-col items-center flex-1"><span className='text-xs text-white'>votes</span><span>{data.no_votes}</span></div>
   <div className="flex flex-col items-center flex-1"><span className='text-xs text-white'>percentage</span><span>{parseFloat(data.percentage.toFixed(1))}%</span></div>
-  <div className="flex-1 bg-accent-one rounded-xl flex items-center justify-center">Vote</div>
+  <button className="flex-1 bg-accent-one rounded-xl flex items-center justify-center " onClick={()=>votec(data._id)}>Vote</button>
 </div>
 </div>
 
-    </>
+    </div>
    )
 })}
  
     </main>
 
       </article>
+      </div>
        {/* ////////////modal////////////// */}
 
-       <div className="">
+      {vote&&<div className="flex fixed flex-col items-center p-4 bg-white w-[90%] h-[60%] top-20 left-6 z-20 rounded-xl pointer-events-auto">
+        <FaTimes className='absolute top-4 right-4 cursor-pointer' size={20} onClick={cancelModal} />
+      <div className="h-16 w-16 rounded-full bg-black "></div>
+      <div className="text-xl text-black">{vote[0].fullname}</div>
+      <div className="flex w-full h-full mt-4 justify-between">
+        <div className="flex-1">
+      <Listbox value={selected} onChange={setSelected} >
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <span className="block truncate">{selected.vote} votes</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <FaChevronDown
+                className="h-3 w-3 text-gray-400"
+                aria-hidden="true"
+                
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-32 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {votes.map((person, personIdx) => (
+                <Listbox.Option
+                  key={personIdx}
+                  className={({ active,focus }) =>
+                    (`relative cursor-default select-none py-2 pl-4 text-xs pr-4 ${
+                      active ? `bg-amber-100 text-amber-900 ${test(person.price)}` : 'text-gray-900'
+                    }${focus?``:``}`)
+                  }
+                  value={person}
+                >
+                  {({ selected }) => 
+                  (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? `font-medium ` : 'font-normal'
+                        }`}
+                      >
+                        {person.vote} votes
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                          <FaCheck className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+      </div>
 
+      <div className="flex-1  flex flex-col justify-center items-center">
+      <div className="text-2xl ">Price:{price}</div>
+      <button className=''>pay now</button>
+      </div>
        </div>
+       </div>}
+
     </div>
   )
 }
